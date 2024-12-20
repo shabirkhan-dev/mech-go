f877380bb340821e3989adb66f9f850982205f98
Shabir <135199362+shabirkhan10@users.noreply.github.com>
2024-12-21 04:44:47 +0500

    feat: initialize new expo project with linter formater and githooks

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Text, View } from "react-native";
+
+const AppHome = () => {
+	return (
+		<View>
+			<Text>App Test</Text>
+		</View>
+	);
+};
+
+export default AppHome;
